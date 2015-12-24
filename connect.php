266c9952a2a1155300d2ea3417cd0d11c3a964ea
Lokesh Kumar J R <lokesh.kumar20012012@live.com>
2015-12-24 11:40:52 +0100

    Simplified DB connection

--- connect.php
+++ connect.php
@@ -5,10 +5,6 @@ $db = 'user';
 $host = 'localhost';
 $port = 8889;
 $success = mysqli_connect($host, $user, $password, $db, $port);
-if(!$success)
-    echo "fail";
-else
-    echo "success";
 ?>
 
 
