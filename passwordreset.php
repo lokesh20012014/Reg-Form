5ae1fcc3b0dc84663135247fa2e503784737dc85
Lokesh Kumar J R <lokesh.kumar20012012@live.com>
2015-12-26 15:40:15 +0100

    Adding Password Reset Form

---
+++ passwordreset.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home: Webpage</title>
+</head>
+<body>
+<form action="" method="post">
+        <label> Enter your User ID : </label>
+        <input id="username" type="text" name="username" />
+        <input id="button" type="submit" name="button" value="Submit" />
+    </form>
+</body>
+</html>
